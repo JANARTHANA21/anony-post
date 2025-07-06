228e7f86b84c21cc4cc2f7a9ac6ea444fa1ef16e
JANARTHANA21 <janarthana.life@gmail.com>
2025-07-06 16:38:05 +0530

    Chore: forced rebuild trigger

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import InputField from "../components/InputField";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import API from "../API/axios.js";
+import API from "../api/axios.js";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
