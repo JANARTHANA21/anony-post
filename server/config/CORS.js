754d137a0e9f4ce618bfaea92cacedb26c650178
JANARTHANA21 <janarthana.life@gmail.com>
2025-07-06 18:22:24 +0530

    fix:CORS file

--- server/config/CORS.js
+++ server/config/CORS.js
@@ -2,7 +2,7 @@ import cors from "cors";
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://your-production-url.com"
+  "https://echohaven-git-main-janarthana-s-projects.vercel.app"
 ];
 
 export default function CORS() {
