ec24b2b85f3501216fd495f9e057b21c51f467a0
JANARTHANA <142612508+JANARTHANA21@users.noreply.github.com>
2025-07-09 09:19:28 +0530

    Update CORS.js
    fix:"added extra domains"

--- server/config/CORS.js
+++ server/config/CORS.js
@@ -2,7 +2,9 @@ import cors from "cors";
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://echohaven-git-main-janarthana-s-projects.vercel.app"
+  "echohaven.vercel.app",
+"echohaven-git-main-janarthana-s-projects.vercel.app",
+"echohaven-foo5ri5r7-janarthana-s-projects.vercel.app"
 ];
 
 export default function CORS() {
