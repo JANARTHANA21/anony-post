1b72a5c6d2826084490b79adeda92d1587afc42a
JANARTHANA <142612508+JANARTHANA21@users.noreply.github.com>
2025-07-09 09:36:02 +0530

    Update CORS.js
    fix:"failed to added https now changed"

--- server/config/CORS.js
+++ server/config/CORS.js
@@ -2,9 +2,9 @@ import cors from "cors";
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "echohaven.vercel.app",
-"echohaven-git-main-janarthana-s-projects.vercel.app",
-"echohaven-foo5ri5r7-janarthana-s-projects.vercel.app"
+  "https://echohaven.vercel.app",
+"https://echohaven-git-main-janarthana-s-projects.vercel.app",
+"https://echohaven-foo5ri5r7-janarthana-s-projects.vercel.app"
 ];
 
 export default function CORS() {
